3e231e61b2adf614dab5a152f5bc565cf41a4d88
David Panofsky <dpanofsky@singleplatform.com>
2017-08-16 15:28:32 -0400

    add limit, asap mode, and summary statistics

--- replayer.py
+++ replayer.py
@@ -9,6 +9,7 @@ from urlparse import urlparse
 from twisted.internet import reactor
 
 description = 'ELB Log Replayer (ELR)'
+totals = {'successful': 0, 'failed': 0}
 
 parser = argparse.ArgumentParser(description=description)
 parser.add_argument('logfile', help='the logfile to replay')
@@ -32,6 +33,16 @@ parser.add_argument(
     action='store_true',
     help='don\'t actually hit the `host`',
 )
+parser.add_argument(
+    '--asap',
+    action='store_true',
+    help='play requests as quickly as possible, not as they were in the original file'
+)
+parser.add_argument(
+    '--limit',
+    action='store',
+    help='replay only first n requests'
+)
 
 script_args = parser.parse_args()
 
@@ -41,21 +52,27 @@ def replay_request(url, host, orig_resp):
         sys.stdout.write('{}\n'.format(url))
     else:
         s = requests.Session()
-        req = requests.Request('GET', url)
+        req = requests.Request('GET', 'http://{}{}?{}'.format(script_args.host, url.path, url.query))
         prepped = req.prepare()
         if script_args.preserve_host:
             prepped.headers['Host'] = host
         resp = s.send(prepped)
         if str(resp.status_code) == str(orig_resp):
             warning = ''
+            totals['successful'] += 1
         else:
             warning = 'WARNING'
-        if script_args.verbose:
-            print '{}=>{} {} {}'.format(orig_resp, resp.status_code, warning, url)
+            totals['failed'] += 1
+        if script_args.verbose or warning == 'WARNING':
+            print '{}=>{} {} {}?{}'.format(orig_resp, resp.status_code, warning, url.path, url.query)
 
 
 def main():
     starting = None
+    if script_args.limit:
+        counter = int(script_args.limit)
+    else:
+        counter = None
     for line in open(script_args.logfile):
         bits = line.split()
         timestamp = dateutil.parser.parse(bits[0])
@@ -71,15 +88,20 @@ def main():
             continue
         orig_host = url.netloc.split(':')[0]
         orig_resp = bits[8]
-        request_path = 'http://{}{}?{}'.format(
-            script_args.host,
-            url.path,
-            url.query
-        )
-        reactor.callLater(offset.total_seconds(), replay_request, request_path, orig_host, orig_resp)
+        if counter == 0:
+            break
+        if script_args.limit:
+            counter -= 1
+        if script_args.asap or script_args.dry_run:
+            replay_request(url, orig_host, orig_resp)
+        else:
+            reactor.callLater(offset.total_seconds(), replay_request, url, orig_host, orig_resp)
 
-    reactor.callLater(offset.total_seconds() + 4, reactor.stop)
-    reactor.run()
+    if not script_args.asap:
+        reactor.callLater(offset.total_seconds() + 4, reactor.stop)
+        reactor.run()
+
+    print totals
 
 if __name__ == "__main__":
     main()
