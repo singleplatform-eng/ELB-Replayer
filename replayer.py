e2d759aad1eb8bdefd0f22b33c215eb015523433
David Panofsky <dpanofsky@singleplatform.com>
2018-01-08 14:27:04 -0500

    pylint cleanups, add dummy arg to account for spdj_gatekeeper_dummy_response

--- replayer.py
+++ replayer.py
@@ -9,79 +9,86 @@ from urlparse import urlparse
 
 from twisted.internet import reactor
 
-description = 'ELB Log Replayer (ELR)'
-totals = {'successful': 0, 'failed': 0}
+SCRIPT_DESCRIPTION = 'ELB Log Replayer (ELR)'
+TOTALS = {'successful': 0, 'failed': 0}
 
-spinner = itertools.cycle(['-', '\\', '|', '/'])
-parser = argparse.ArgumentParser(description=description)
-parser.add_argument('logfile', help='the logfile to replay')
-parser.add_argument(
+SPINNER = itertools.cycle(['-', '\\', '|', '/'])
+PARSER = argparse.ArgumentParser(description=SCRIPT_DESCRIPTION)
+PARSER.add_argument('logfile', help='the logfile to replay')
+PARSER.add_argument(
     '--host',
     help='host to send requests',
     default='localhost',
 )
-parser.add_argument(
+PARSER.add_argument(
     '--replace-host',
     action='store_false',
     help='use host parameter for request Host header'
 )
-parser.add_argument(
+PARSER.add_argument(
     '--verbose',
     action='store_true',
     help='always print requests and responses',
 )
-parser.add_argument(
+PARSER.add_argument(
     '--dry-run',
     action='store_true',
     help='don\'t actually hit the `host`',
 )
-parser.add_argument(
+PARSER.add_argument(
     '--paced',
     action='store_true',
     help='play requests as quickly as possible, not as they were in the original file'
 )
-parser.add_argument(
+PARSER.add_argument(
     '--limit',
     action='store',
     help='replay only first n requests'
 )
+PARSER.add_argument(
+    '--dummy',
+    action='store_true',
+    help='treat 404=>200 transitions as successes (for when spdj_gatekeeper_dummy_response is true)'
+)
 
-script_args = parser.parse_args()
+SCRIPT_ARGS = PARSER.parse_args()
 
 
 def replay_request(url, host, orig_resp):
-    if not script_args.verbose:
+    if not SCRIPT_ARGS.verbose:
         sys.stdout.write('\b')
-        sys.stdout.write(spinner.next())
+        sys.stdout.write(SPINNER.next())
         sys.stdout.flush()
-    if script_args.dry_run:
+    if SCRIPT_ARGS.dry_run:
         sys.stdout.write('{}\n'.format(url))
     else:
-        s = requests.Session()
-        req = requests.Request('GET', 'http://{}{}?{}'.format(script_args.host, url.path, url.query))
+        session = requests.Session()
+        req = requests.Request('GET', 'http://{}{}?{}'.format(SCRIPT_ARGS.host, url.path, url.query))
         prepped = req.prepare()
-        if script_args.replace_host:
+        if SCRIPT_ARGS.replace_host:
             prepped.headers['Host'] = host
-        resp = s.send(prepped)
+        resp = session.send(prepped)
+        if SCRIPT_ARGS.dummy and int(orig_resp) == 404 and int(resp.status_code) == 200:
+            resp.status_code = 404
         if str(resp.status_code) == str(orig_resp):
             warning = ''
-            totals['successful'] += 1
+            TOTALS['successful'] += 1
         else:
             warning = 'WARNING'
-            totals['failed'] += 1
-        if script_args.verbose or warning == 'WARNING':
+            TOTALS['failed'] += 1
+        if SCRIPT_ARGS.verbose or warning == 'WARNING':
             print '{}=>{} {} {}?{}'.format(orig_resp, resp.status_code, warning, url.path, url.query)
 
 
 def main():
     starting = None
-    if script_args.limit:
-        countdown = int(script_args.limit)
+    if SCRIPT_ARGS.limit:
+        countdown = int(SCRIPT_ARGS.limit)
     else:
         countdown = None
-    for line in open(script_args.logfile):
+    for line in open(SCRIPT_ARGS.logfile):
         bits = line.split()
-        if script_args.paced:
+        if SCRIPT_ARGS.paced:
             timestamp = dateutil.parser.parse(bits[0])
             if not starting:
                 starting = timestamp
@@ -97,24 +104,24 @@ def main():
         orig_resp = bits[8]
         if countdown == 0:
             break
-        if script_args.limit:
+        if SCRIPT_ARGS.limit:
             countdown -= 1
-        if script_args.paced:
+        if SCRIPT_ARGS.paced:
             reactor.callLater(offset.total_seconds(), replay_request, url, orig_host, orig_resp)
         else:
             reactor.callInThread(replay_request, url, orig_host, orig_resp)
 
-    if script_args.paced:
+    if SCRIPT_ARGS.paced:
         reactor.callLater(offset.total_seconds() + 2, reactor.stop)
     else:
         reactor.callFromThread(reactor.stop)
 
     reactor.run()
 
-    print totals
+    print TOTALS
 
 if __name__ == "__main__":
-    print script_args
-    if script_args.dry_run:
-        script_args.paced = False
+    print SCRIPT_ARGS
+    if SCRIPT_ARGS.dry_run:
+        SCRIPT_ARGS.paced = False
     main()
